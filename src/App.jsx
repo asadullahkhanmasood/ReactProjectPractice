3b8f4cba00b1ded034bd351cc04a44c2793da159
asadullahn1213 <roshan23345ali@gmail.com>
2025-11-23 17:24:40 +0500

    folders-created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import './App.css'
+import React from 'react'
+import Navbar from './components/navbar/navbar'
+// import "react-router" from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    <header className="">
+        <div className="logo">
+            <h2>LOGO</h2>
+        </div>
+
+        <div className="links-items">
+            <ul>
+                <li><a href="http://" target="_blank" rel="noopener noreferrer">HOME</a></li>
+                <li><a href="http://" target="_blank" rel="noopener noreferrer">ABOUT</a></li>
+                <li><a href="http://" target="_blank" rel="noopener noreferrer">CONTACT</a></li>
+                <li><a href="http://" target="_blank" rel="noopener noreferrer">PROJECT</a></li>
+            </ul>
+        </div>
+
+
+
+    </header>
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Navbar
