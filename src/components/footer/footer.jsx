bbcbcd7f4ec6c2beda2cb0f2d1d7a4eec7183c25
asadullahn1213 <roshan23345ali@gmail.com>
2025-11-29 07:47:53 +0500

    foooter completed :

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,9 +3,52 @@ import React from 'react'
 function Footer() {
   return (
 <>
-<div className="footer md:w-full md:bg-black md:mt-20 md:h-100">
-    <div className="footer-box bg-white md:w-[80%] md:mx-auto md:h-[90%]">
-        
+<div className="footer md:w-full md:bg-black md:mt-20 md:h-70">
+    <div className="footer-box  md:w-[80%] md:mx-auto md:h-[90%] md:py-10">
+       <div className="item-footer-box flex justify-between mx-10 h-full">
+        {/* box width 1  */}
+         <div className="w-90 flex items-center justify-center h-full    ">
+          <ul className='h-full items-center justify-between text-white flex flex-col'>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">HOME</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">ABOUT ME</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">CONTACT ME</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">CONTACT ME</a></li>
+
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">CONTACT ME</a></li>
+
+          </ul>
+        </div>
+
+        {/* 1 complte  */}
+           <div className="w-90 flex items-center justify-center h-full    ">
+          <ul className='h-full items-center justify-between text-white  flex flex-col'>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">HOME</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">ABOUT ME</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">CONTACT ME</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">CONTACT ME</a></li>
+
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">CONTACT ME</a></li>
+
+          </ul>
+        </div>
+
+        {/* box end  */}
+           <div className="w-90 flex items-center justify-center h-full    ">
+          <ul className='h-full items-center justify-between text-white  flex flex-col'>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">HOME</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">ABOUT ME</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">CONTACT ME</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">CONTACT ME</a></li>
+
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">CONTACT ME</a></li>
+
+          </ul>
+        </div>
+       </div>
+    </div>
+
+    <div className="reserve-text md:w-full md:justify-center md:items-center flex">
+      <h3 className='text-white'>Lorem ipsum dolor sit amet, con Harum reut eos ovident vitae dolores magnam aliquid quibusdam!</h3>
     </div>
 </div>
 
